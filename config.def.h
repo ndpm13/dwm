a5a5787425d7cb420d5d14109221b48362168766
Naz <ndpm13@ch-naseem.com>
2025-10-15 15:23:39 +0100

    ✨feat: apply dwm-splitstatus-20201114-61bb8b2 patch

--- config.def.h
+++ config.def.h
@@ -11,6 +11,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
